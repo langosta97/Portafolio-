ec986a5a90d6546090a8e34b6c88761d4d3ba662
Katherinne Barahona Ulloa <katherinnebarahonaulloa@gmail.com>
2024-08-26 16:36:44 -0400

    se adjuntaron imagenes en cada cuadro, se creo una ventana oculta con js, y se creo una pagina de redirección

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
-console.log("hola")
+var mostrarLista = document.querySelector("#botonAbrir")
+
+var mostrarLista = () =>{
+    lista.style.display = "block"
+}
+
+botonAbrir.onclick = mostrarLista
+
+var cerrarLista = document.querySelector("#botonCerrar")
+
+var cerrarLista = () =>{
+    lista.style.display = "none"
+}
+
+botonCerrar.onclick = cerrarLista
+
+$( function() {
+    $("#dialog").dialog();
+  } );
